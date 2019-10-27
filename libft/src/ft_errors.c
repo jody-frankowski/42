f7af263a42a92af42cdde4853c39477c2940f933
Jody Frankowski <jody.frankowski@gmail.com>
2019-10-27 14:37:38 +0100

    libft: document ft_err_exit

--- libft/src/ft_errors.c
+++ libft/src/ft_errors.c
@@ -14,6 +14,13 @@
 #include <stdlib.h>
 #include "libft.h"
 
+/*
+** ft_err_exit - Output an error message and exit depending on a value
+** @statement_value: The value to check for
+**
+** ft_err_exit will output @error_message and exit when @statement_value is true
+*/
+
 void	ft_err_exit(int statement_value, const char *error_message)
 {
 	if (statement_value)
