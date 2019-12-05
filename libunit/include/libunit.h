1670b36446997155c0aa51155bb46a1dcbb6eb99
Jody Frankowski <jody.frankowski@gmail.com>
2019-12-05 22:37:36 +0100

    libunit: add ASSERT* macros

--- libunit/include/libunit.h
+++ libunit/include/libunit.h
@@ -14,11 +14,87 @@
 #ifndef LIBUNIT_H
 # define LIBUNIT_H
 
+# include <stdio.h>
 # include <string.h>
 
 # define TEST_TIMEOUT "Timeout"
 
 # define RUN_TEST(func) run_test(func, #func, __FILE__, __LINE__)
+
+/*
+** ASSERT - Assert something and print an error if false
+** @left:  The first operand
+** @op:    A C operator
+** @right: The second operand
+*/
+# define ASSERT(left, op, right) do { \
+	typeof(left) _left = (left); \
+	typeof(right) _right = (right); \
+	if (!(_left op _right)) \
+	{ \
+		dprintf( \
+			2, \
+			"%s@(%s:%d): %s !%s %s (%#llx !%s %#llx)\n", \
+			__func__, __FILE__, __LINE__, #left, #op, #right, (unsigned long long)_left, #op, (unsigned long long)_right); \
+	} \
+} while (0)
+
+/*
+** ASSERT_OBJ - Assert something about two objects and print them if error
+** @cmp:   A function that compares @left and @right, called like so:
+**         cmp(fd, typeof(left|right))
+** @left:  The first operand.
+** @right: The second operand
+** @op:    A C expression that will make "cmp(left, right) op" evaluate properly
+** @print: A function that can print @left and @right and takes a file
+**         descriptor as a first argument, called like so:
+**         print(fd, typeof(left|right), @len)
+*/
+
+# define ASSERT_OBJ(cmp, left, right, op, print) do { \
+	typeof(left) _left = (left); \
+	typeof(right) _right = (right); \
+	if (!(cmp(_left, _right)) op) \
+	{ \
+		dprintf( \
+			2, \
+			"%s@(%s:%d): %s(%s, %s) !%s:\n", \
+			__func__, __FILE__, __LINE__, #cmp, #left, #right, #op); \
+		print(2, _left); \
+		dprintf(2, "\n"); \
+		print(2, _right); \
+		dprintf(2, "\n"); \
+	} \
+} while (0)
+
+/*
+** ASSERT_NOBJ - Assert something about two objects and print them if error
+** @cmp:   A function that compares @left and @right, called like so:
+**         cmp(fd, typeof(left|right), @len)
+** @left:  The first operand.
+** @right: The second operand
+** @len:   A length to limit the @cmp and @print functions
+** @op:    A C expression that will make "cmp(left, right) op" evaluate properly
+** @print: A function that can print @left and @right and takes a file
+**         descriptor as a first argument, called like so:
+**         print(fd, typeof(left|right), @len)
+*/
+
+# define ASSERT_NOBJ(cmp, left, right, len, op, print) do { \
+	typeof(left) _left = (left); \
+	typeof(right) _right = (right); \
+	if (!(cmp(_left, _right, len)) op) \
+	{ \
+		dprintf( \
+			2, \
+			"%s@(%s:%d): %s(%s, %s) !%s:\n", \
+			__func__, __FILE__, __LINE__, #cmp, #left, #right, #op);	\
+		print(2, _left, len);											\
+		dprintf(2, "\n"); \
+		print(2, _right, len);		   \
+		dprintf(2, "\n"); \
+	} \
+} while (0)
 
 # define ASSERT_MEM_EQ(a, b, len) \
 	assert_mem_eq(#a, #b, __FILE__, __func__, __LINE__, a, b, len)
