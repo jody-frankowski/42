abb45c51231cb58a76446d5dd971516953ecf0c2
Jody Frankowski <jody.frankowski@gmail.com>
2020-05-02 14:13:57 +0200

    feat(libft): Document the trim functions

--- libft/src/ft_trim.c
+++ libft/src/ft_trim.c
@@ -13,12 +13,34 @@
 #include <stddef.h>
 #include "libft.h"
 
+/*
+** ft_ltrim() - Remove leading characters from a string
+** @str:   The string to treat
+** @chars: The characters to remove
+**
+** ft_ltrim() will "remove" leading characters by returning a pointer to the
+** first character that isn't in @chars.
+**
+** Return: A pointer in @str after the leading characters.
+*/
+
 char	*ft_ltrim(char *str, char *chars)
 {
 	while (*str && ft_incharset(*str, chars))
 		str++;
 	return (str);
 }
+
+/*
+** ft_rtrim() - Remove trailing characters from a string
+** @str:   The string to treat
+** @chars: The characters to remove
+**
+** ft_rtrim() will "remove" trailing characters by overwriting them with \0
+** characters.
+**
+** Return: A pointer to @str.
+*/
 
 char	*ft_rtrim(char *str, char *chars)
 {
@@ -32,6 +54,17 @@ char	*ft_rtrim(char *str, char *chars)
 	}
 	return (str);
 }
+
+/*
+** ft_trim() - Remove leading and trailing characters from a string
+** @str:   The string to treat
+** @chars: The characters to remove
+**
+** ft_trim() will "remove" leading and trailing characters by calling ft_ltrim()
+** and ft_rtrim() on @str.
+**
+** Return: A pointer in @str after the leading characters.
+*/
 
 char	*ft_trim(char *str, char *chars)
 {
